b239f2c2b06576e6ee79202a82064429ad4b5c6d
Eccenux <eccenux+git@gmail.com>
2024-01-08 18:28:28 +0100

    column order

--- .sql/check.sql
+++ .sql/check.sql
@@ -1,1 +1,10 @@
-select * from csp_reports
+SELECT id, created_at
+, document_uri
+, effective_directive
+, violated_directive
+, disposition
+, status_code
+, blocked_uri
+, referrer
+
+FROM public.csp_reports;
--- .sql/create.sql
+++ .sql/create.sql
@@ -9,14 +9,16 @@ CREATE DATABASE stats_csp;
 -- Tworzenie tabeli do przechowywania raportów CSP
 -- DROP TABLE IF EXISTS csp_reports;
 CREATE TABLE IF NOT EXISTS csp_reports (
-    id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    blocked_uri VARCHAR(255),
-    disposition VARCHAR(50),
-    document_uri VARCHAR(255),
-    effective_directive VARCHAR(50),
-    original_policy TEXT,
-    referrer VARCHAR(255),
-    status_code INTEGER,
-    violated_directive VARCHAR(50)
+
+	document_uri VARCHAR(255),
+	effective_directive VARCHAR(50),
+	violated_directive VARCHAR(50),
+
+	disposition VARCHAR(50),
+	status_code INTEGER,
+	blocked_uri VARCHAR(255),
+	original_policy TEXT,
+	referrer VARCHAR(255)
 );
