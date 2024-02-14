a8e2d45c14b6e731545d08df13b4af615eecf491
Eccenux <eccenux+git@gmail.com>
2024-02-14 14:38:51 +0100

    order

--- .sql/check.sql
+++ .sql/check.sql
@@ -1,10 +1,14 @@
 SELECT id, created_at
 , document_uri
+
 , effective_directive
 , violated_directive
+
 , disposition
 , status_code
 , blocked_uri
 , referrer
 
-FROM public.csp_reports;
+FROM public.csp_reports
+order by id desc
+;
